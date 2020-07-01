885ff17cbebb7767643e0bc9b98e6ce9ad153625
iosorin <miroslav.osorin@gmail.com>
2020-07-01 10:41:08 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+        ecmaVersion: 2019,
+        sourceType: 'module'
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
+    ],
+    plugins: [
+    ],
+    // add your custom rules here
+    rules: {
+        'semi': [2, 'always'],
+        'no-console': 1,
+        'quote-props': [0, 'as-needed'],
+        'no-unused-expressions': 2,
+        'no-unused-vars': 1,
+        'indent': [
+            1,
+            4,
+            {
+                'ignoredNodes': [
+                    'TemplateLiteral'
+                ],
+                'SwitchCase': 1
+            }
+        ],
+        'vue/max-attributes-per-line': 0,
+        'vue/html-indent': [
+            1, 4, {
+                'attribute': 1,
+                'baseIndent': 1,
+                'closeBracket': 0,
+                'alignAttributesVertically': false,
+                'ignores': []
+            }
+        ]
+    }
+};
